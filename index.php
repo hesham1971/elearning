295d7a9802d6c24dfd29c684d31d6c8ca95d9a3a
hesham1971 <144853439+hesham1971@users.noreply.github.com>
2023-09-13 23:30:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-//$docRoot = $_SERVER['DOCUMENT_ROOT'];
-//$path = "https://".$docRoot."/index.php";
-//$path = "http://sql204.iceiy.com/htdocs/uploads/Bhie_Logo.png';
 echo "Welcome";
-//echo base_url;
-//echo '<a href= "https://sql204.iceiy.com/htdocs/uploads/slides/lesson_1/Slide2.jpg"> "slide1" </a>';
-//echo '<a href= "https://'.$path.'"/Bhie_Logo.png"> "slide1" </a>';
-//echo '<a href= "https://elearn/Bhie_Logo.png"> "slide1" </a>';
+echo '<a href= "https://hesham1971.github.io/elearning/Bhie_Logo.png"> "slide1" </a>';
 echo '<a href= "https://www.google.com"> "google" </a>';
 ?>
